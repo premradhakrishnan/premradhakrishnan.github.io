f32bb232a31f1628a47da8a804a4419c91e7f20c
Prem Radhakrishnan [SSW] <58406844+premradhakrishnan@users.noreply.github.com>
2022-05-02 05:29:09 +1000

    Ghost script

---
+++ assets/js/ghost.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("a[href^=http]").each(function(){
+       var excluded = [
+          // format for whitelist: 'google.com', 'apple.com', 'myawesomeblog.com'
+          // add your excluded domains here
+          ];
+       for(i=0; i<excluded.length; i++) {
+          if(this.href.indexOf(excluded[i]) != -1) {
+             return true;
+          }
+       }
+       if(this.href.indexOf(location.hostname) == -1) {
+            $(this).click(function() { return true; }); 
+            $(this).attr({
+                target: "_blank",
+                title: "Opens in a new window"
+            });
+            $(this).click();
+       }
+    })
+ });
